0479a5e634d9a0127d5d0b8b7412d24774ba5a26
Lanielm <203065349+Lanielm@users.noreply.github.com>
2026-08-18 11:26:34 -0300

    default to ignore pets and jars

--- src/main/java/com/bluelog/BlueLogConfig.java
+++ src/main/java/com/bluelog/BlueLogConfig.java
@@ -20,12 +20,12 @@ public interface BlueLogConfig extends Config {
 
 	@ConfigItem(keyName = "ignoreAllPets", name = "Ignore all pets", description = "Ignores pets from all sections", position = 2, section = itemsSection)
 	default boolean ignoreAllPets() {
-		return false;
+		return true;
 	}
 
 	@ConfigItem(keyName = "ignoreAllJars", name = "Ignore all jars", description = "Ignores jars from all sections", position = 3, section = itemsSection)
 	default boolean ignoreAllJars() {
-		return false;
+		return true;
 	}
 
 	@ConfigItem(keyName = "markerCorner", name = "Marker corner", description = "Corner of the item the ignored marker is drawn in", position = 4, section = itemsSection)
